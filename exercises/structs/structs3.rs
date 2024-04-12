4d6dde1cbb7fd4f1643874fbba7f5f84cd46ecbe
lx1749 <lighthouse@localhost.localdomain>
2024-04-13 00:01:40 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -17,6 +17,7 @@ struct Package {
 }
 
 impl Package {
+    ///struct associated function
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             panic!("Can not ship a weightless package.")
@@ -28,13 +29,13 @@ impl Package {
             }
         }
     }
-
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    ///method, "&self" equals "self: &Self"
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
