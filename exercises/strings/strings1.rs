3c7a728da527a78a2c52d5f4253480f7ecfd9331
lx1749 <lighthouse@localhost.localdomain>
2024-04-13 14:41:09 +0800

    update

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,7 +5,7 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let answer = current_favorite_color();
@@ -13,5 +13,7 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    //字符串字面量是预分配文本的 &str,该文本存储在只读内存区
+    //本函数应返回owned string，可采用.to_string()方法，该方法生成一个新的owned string,具有和&str相同的文本串
+    "blue".to_string()
 }
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,11 +5,10 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
@@ -17,5 +16,12 @@ fn main() {
 }
 
 fn is_a_color_word(attempt: &str) -> bool {
+    //比较运算符会追踪两边的所有引用并对最终目标a,b进行比较
+    //如(a,b为相同类型的值):
+    //&a == &b <=> a == b      &&a == &&b <=> a == b
+    //但比较运算符的操作数必须具有相同的类型
+    //若 &a == &&b 则会出现错误 
+    //综上，对两个引用的比较实际比较的是引用目标
+    //若确实希望比较两个引用是否指向同一块地址，可使用std::ptr::eq(&a, &b)
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
