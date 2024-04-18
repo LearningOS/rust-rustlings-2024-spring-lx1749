803a4542ff657093ac57dd108da6d87ec215f4a8
lx1749 <lighthouse@localhost.localdomain>
2024-04-18 13:51:46 +0800

    update

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -7,25 +7,28 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::Arc;
+
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 struct JobStatus {
     jobs_completed: u32,
-}
+}   
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
+        //Arc的引用计数的增减是原子性的
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            //Result<T,Error>.unwrap()的返回值类型为T,因为若为Err(e)则会引发panic故不影响返回值类型
+            //所以这儿可以直接  .unwrap().jobs_completed
+            status_shared.lock().unwrap().jobs_completed += 1;
         });
         handles.push(handle);
     }
@@ -34,6 +37,7 @@ fn main() {
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
         // anything interesting in the output? Do you have to 'join' on all the
         // handles?
-        println!("jobs completed {}", ???);
+
+        println!("jobs completed {}", status.lock().unwrap().jobs_completed);
     }
 }
